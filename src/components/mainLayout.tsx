2e07630f701ee4ae263fd3e1bd6270496e549871
CristianGonzalez07 <1995.cgonzalez.07@gmail.com>
2023-06-15 13:16:39 -0300

    mainLayout modified to pass title to topbar

--- src/components/mainLayout.tsx
+++ src/components/mainLayout.tsx
@@ -2,10 +2,10 @@ import { Section, TopBar } from "../components";
 import { Box } from "@mui/material";
 import { ReactNode } from 'react';
 
-const MainLayout = ({ children }: { children: ReactNode }) => {
+const MainLayout = ({ children , headerTitle}: { children: ReactNode, headerTitle:string }) => {
     return (
         <Box sx={{ height: "100vh", minWidth: "500px", overflow: "scroll" }}>
-            <TopBar />
+            <TopBar title={headerTitle}/>
             <Section>
                 {children}
             </Section>
